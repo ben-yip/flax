c2c25d11e971c46a762cc06f0a6592c2c60f643e
Benjamin Yip <endless_leaf@163.com>
2016-08-25 10:28:19 +0800

    add browser-sync startup batch

---
+++ browser-sync_start.bat
@@ -0,0 +1,6 @@
+::run in project root directory.
+
+browser-sync start --server --port 8000 --ui-port 8001 --no-open --no-notify --files "*.*, pages/*.html, pages.m/*.html, test/*.html, ./*/*.*, ./*/*/*.*"
+
+@echo off
+pause
