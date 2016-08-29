3b02a3c5e59776166fe1e982e5f62e79433029ff
BenYip <endless_leaf@163.com>
2016-08-29 17:06:30 +0800

    Learning Sass and Compass

--- learnSass/config.rb
+++ learnSass/config.rb
@@ -7,19 +7,24 @@ css_dir = "stylesheets"
 sass_dir = "sass"
 images_dir = "images"
 javascripts_dir = "javascripts"
+fonts_dir = "fonts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
+output_style = :expanded
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
 
+# The environment mode. Defaults to :development, can also be :production
+# environment = :production
 
 # If you prefer the indented syntax, you might want to regenerate this
 # project again passing --syntax sass, or you can uncomment this:
 # preferred_syntax = :sass
 # and then run:
 # sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+
