186d17f380aeadf04a916b43c6f918268358a952
BenYip <endless_leaf@163.com>
2016-09-05 15:06:39 +0800

    Compass learning comes to an end temporarily.

--- learnSass/config.rb
+++ learnSass/config.rb
@@ -11,13 +11,16 @@ fonts_dir = "fonts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
-output_style = :expanded
+# output_style = :expanded
+# output_style = :nested
+# output_style = :compact
+# output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
+line_comments = false
 
 # The environment mode. Defaults to :development, can also be :production
 # environment = :production
