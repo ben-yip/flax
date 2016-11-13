4d10b8199266c36530afe06037ac5343ade6ee3a
ben-yip <endless_leaf@163.com>
2016-11-13 16:33:00 +0800

    Amend compass config, remove line comments.

--- config.rb
+++ config.rb
@@ -23,7 +23,7 @@ relative_assets = true
 
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
+ line_comments = false
 
 
 # If you prefer the indented syntax, you might want to regenerate this
